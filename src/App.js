ebe9795e4296664b9454f31f7d613fd41d853fc5
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-31 18:47:56 +0100

    add detail info

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Stock from './components/stock/Stock';
+import DetailsComponent from './pages/DetailsComponent';
 
 function App() {
   return (
     <div className="App">
-      Hello world
-      <Stock />
+      <Routes>
+        <Route exact path="/" element={<Stock />} />
+        <Route path="/details/:companyId" element={<DetailsComponent />} />
+      </Routes>
     </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './Redux/ConfigStore';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -13,5 +15,4 @@ ReactDOM.render(
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
